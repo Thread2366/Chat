8620acea8b75e327a0a23d327ecb45ef687894e1
virus0742 <virus0742@gmail.com>
2020-06-03 18:25:53 +0300

    Tests

---
+++ ChatManager/AuthorizedUser.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatManager
+{
+    public class AuthorizedUser
+    {
+        private ChatContext context;
+
+        public UserInfo UserInfo { get; }
+
+        public AuthorizedUser(ChatContext context, UserInfo user)
+        {
+            this.context = context;
+            UserInfo = user;
+        }
+
+        public bool SendMessage(string username, string messageText)
+        {
+            UserInfo receiver = context.UserInfos.SingleOrDefault(user => user.Username == username);
+            if (receiver == null || string.IsNullOrWhiteSpace(messageText))
+                return false;
+            context.Messages.Add(new Message()
+            {
+                Sender = UserInfo,
+                Receiver = receiver,
+                Text = messageText,
+                Timestamp = DateTime.Now
+            });
+            context.SaveChanges();
+            return true;
+        }
+
+        public List<Message> GetMessages()
+        {
+            IQueryable<Message> messagesQuery = from message in context.Messages
+                                                where message.Sender.Id == UserInfo.Id || message.Receiver.Id == UserInfo.Id
+                                                orderby message.Timestamp
+                                                select message;
+            //IQueryable<Message> messagesQuery = context.Messages.
+            //    Where(m => m.Sender == currentUser || m.Reveiver == currentUser)
+            //    .OrderBy(ms => ms.Timestamp);
+            //IQueryable<Message> messagesQuery = context.Messages.Where(message => message.Sender == currentUser || message.Reveiver == currentUser);
+
+            return messagesQuery.ToList();
+        }
+
+        public List<Message> GetMessages(DateTime start, DateTime finish)
+        {
+            IQueryable<Message> messagesQuery = from message in context.Messages
+                                                where (message.Sender.Id == UserInfo.Id || message.Receiver.Id == UserInfo.Id) && message.Timestamp >= start && message.Timestamp < finish
+                                                orderby message.Timestamp
+                                                select message;
+            //IQueryable<Message> messagesQuery = context.Messages.
+            //    Where(m => m.Sender == currentUser || m.Reveiver == currentUser)
+            //    .OrderBy(ms => ms.Timestamp);
+            //IQueryable<Message> messagesQuery = context.Messages.Where(message => message.Sender == currentUser || message.Reveiver == currentUser);
+
+            return messagesQuery.ToList();
+        }
+
+        public List<UserInfo> GetAllUsers()
+        {
+            IQueryable<UserInfo> usersQuery = from user in context.UserInfos
+                                            where user.Id != UserInfo.Id
+                                            select user;
+            return usersQuery.ToList();
+        }
+
+
+        //Далее функции для работы с группами
+
+        public bool CreateGroup(string groupName, GroupAccessMode accessMode)
+        {
+            var group = new Group()
+            {
+                Name = groupName,
+                AccessMode = accessMode,
+                Administrator = UserInfo
+            };
+            context.Groups.Add(group);
+            context.Participations.Add(new Participation()
+            {
+                Group = group,
+                LastRead = DateTime.Now,
+                User = UserInfo
+            });
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool EnterGroup(Group group)
+        {
+            var groupMember = context.Participations
+                .SingleOrDefault(p => p.Group.Id == group.Id && p.User.Id == UserInfo.Id);
+            if (groupMember != null) return false;
+            context.Participations.Add(new Participation()
+            {
+                Group = group,
+                User = UserInfo,
+                LastRead = DateTime.Now
+            });
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool LeaveGroup(Group group)
+        {
+            if (group.Administrator.Id == UserInfo.Id)
+            {
+                var anotherMember = context.Participations
+                    .FirstOrDefault(p => p.Group.Id == group.Id && p.User.Id != UserInfo.Id);
+                if (anotherMember == null)
+                    context.Groups.Remove(group);
+                else group.Administrator = anotherMember.User;
+            }
+            var groupMember = context.Participations
+                .SingleOrDefault(p => p.Group.Id == group.Id && p.User.Id == UserInfo.Id);
+            if (groupMember == null) return false;
+            context.Participations.Remove(groupMember);
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool MakeAnAdministrator(Group group, UserInfo user)
+        {
+            if (group.Administrator.Id != UserInfo.Id) return false;
+            if (context.Participations
+                .SingleOrDefault(p => p.Group.Id == group.Id && p.User.Id == user.Id) == null)
+                return false;
+            group.Administrator = user;
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool SendGroupMessage(Group group, string text)
+        {
+            var groupMember = context.Participations
+                .SingleOrDefault(p => p.Group.Id == group.Id && p.User.Id == UserInfo.Id);
+            if (groupMember == null) return false;
+            context.GroupMessages.Add(new GroupMessage()
+            {
+                Text = text,
+                Group = group,
+                Sender = UserInfo,
+                Timestamp = DateTime.Now
+            });
+            context.SaveChanges();
+            return true;
+        }
+
+        public List<GroupMessage> GetGroupMessages(Group group, bool unread)
+        {
+            var groupMember = context.Participations
+                .SingleOrDefault(p => p.Group.Id == group.Id && p.User.Id == UserInfo.Id);
+            if (groupMember == null) return null;
+            var messages = context.GroupMessages
+                .Where(msg => msg.Group.Id == group.Id);
+
+            var result = unread ?
+                messages.Where(msg => msg.Timestamp > groupMember.LastRead).ToList() :
+                messages.ToList();
+
+            groupMember.LastRead = DateTime.Now;
+            context.SaveChanges();
+            return result;
+        }
+
+        public List<Group> GetAvailableGroups()
+        {
+            return context.Groups
+                .Where(grp => grp.AccessMode == GroupAccessMode.Public)
+                .ToList();
+        }
+
+        public List<Group> GetUserGroups()
+        {
+            return context.Participations
+                .Where(p => p.User.Id == UserInfo.Id)
+                .Select(p => p.Group)
+                .ToList();
+        }
+
+        public bool DeleteGroup(Group group)
+        {
+            if (group.Administrator.Id != UserInfo.Id) return false;
+            context.Groups.Remove(group);
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteMessage(GroupMessage message)
+        {
+            if (message.Group.Administrator.Id != UserInfo.Id) return false;
+            context.GroupMessages.Remove(message);
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool AddToGroup(Group group, UserInfo user)
+        {
+            if (user.Id == UserInfo.Id) return false;
+            if (group.Administrator.Id != UserInfo.Id)
+                return false;
+            context.Participations.Add(new Participation()
+            {
+                Group = group,
+                User = user,
+                LastRead = DateTime.Now
+            });
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ ChatManager/ChatManagerTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using NUnit.Framework;
+
+namespace ChatManager
+{
+    [TestFixture]
+    public static class ChatManagerTests
+    {
+        static MessageManager manager = new MessageManager();
+
+        [Test]
+        public static void CreateGroupTest()
+        {
+            manager.Register("username1", "password1");
+            var user = manager.Authorize("username1", "password1");
+            user.CreateGroup("group1", GroupAccessMode.Public);
+        }
+
+        [Test]
+        public static void EnterGroupTest()
+        {
+            //manager.Register("username2", "password2");
+            var user = manager.Authorize("username2", "password2");
+            var group = user.GetAvailableGroups().First(g => g.Name == "group1");
+            user.EnterGroup(group);
+            user.EnterGroup(group);
+        }
+
+        [Test]
+        public static void SendGroupMessageTest()
+        {
+            var user = manager.Authorize("username2", "password2");
+            var group = user.GetAvailableGroups().First(g => g.Name == "group1");
+            user.SendGroupMessage(group, "some text");
+        }
+
+        [Test]
+        public static void GetGroupMessagesTest()
+        {
+            var user = manager.Authorize("username2", "password2");
+            var group = user.GetUserGroups().First(g => g.Name == "group1");
+            var messages = user.GetGroupMessages(group, true);
+            messages = user.GetGroupMessages(group, true);
+            messages = user.GetGroupMessages(group, false);
+        }
+
+        [Test]
+        public static void GetUserGroupsTest()
+        {
+            var user = manager.Authorize("username2", "password2");
+            user.CreateGroup("group2", GroupAccessMode.Private);
+            var groups = user.GetUserGroups();
+        }
+
+        [Test]
+        public static void DeleteGroupTest()
+        {
+            var user = manager.Authorize("username2", "password2");
+            var group = user.GetUserGroups().First(g => g.Name == "group2");
+            user.DeleteGroup(group);
+        }
+
+        [Test]
+        public static void LeaveGroupTest()
+        {
+            var user = manager.Authorize("username2", "password2");
+            var group = user.GetUserGroups().First(g => g.Name == "group1");
+            user.LeaveGroup(group);
+        }
+
+        [Test]
+        public static void MakeAnAdministratorTest()
+        {
+            var user = manager.Authorize("username1", "password1");
+            var group = user.GetUserGroups().First(g => g.Id == 3);
+            var anotherUser = user.GetAllUsers().First(u => u.Id == 2);
+            user.MakeAnAdministrator(group, anotherUser);
+        }
+
+        [Test]
+        public static void AddToGroupTest()
+        {
+            var user = manager.Authorize("username2", "password2");
+            var group = user.GetUserGroups().First(g => g.Id == 5);
+            var anotherUser = user.GetAllUsers().First(u => u.Id == 1);
+            user.AddToGroup(group, anotherUser);
+        }
+    }
+}
